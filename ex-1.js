690e3a5df907b5288ec9644e3d4c06ef7ba207c7
Kanokphit <kanokphit55@gmail.com>
2024-05-17 15:03:58 +0700

    JavaScript Callbacks and Higher Order Functions Assignment

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,18 @@
 
 function forEach(array, operation) {
   // Start coding here
+  for (let num of array) {
+    operation(num);
+  }
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
+function plusSalaries(num) {
+  newEmployeeSalaries.push(num + 5000);
+}
 
+forEach(employeeSalaries, plusSalaries);
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,7 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  return operation(array.filter((score) => score > 70));
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -9,7 +10,27 @@ const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
+function standard(count) {
+  if (count.length >= 5) {
+    return "ผ่านเกณฑ์ ✅";
+  } else {
+    return "ไม่ผ่านเกณฑ์ ❌";
+  }
+}
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = `นักเรียนห้องที่ 1 ${atLeastFive(
+  studentScoresRoom1,
+  standard
+)}`;
+let scoreRoom2Result = `นักเรียนห้องที่ 2 ${atLeastFive(
+  studentScoresRoom2,
+  standard
+)}`;
+let scoreRoom3Result = `นักเรียนห้องที่ 3 ${atLeastFive(
+  studentScoresRoom3,
+  standard
+)}`;
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
